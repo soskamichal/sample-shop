ce5862f28b4b9c15c8706d4c2a1ed33132dd9fd3
soskamichal <michalkososka@gmail.com>
2021-12-19 00:15:43 +0100

    category be connection (no-cors)

--- src/components/CategorySelection.vue
+++ src/components/CategorySelection.vue
@@ -16,96 +16,20 @@
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 export default {
   data () {
     return {
-      categories: [
-        {
-          uid: 'Mjg=',
-          name: 'Bottoms',
-          children: [
-            {
-              uid: 'MzE=',
-              name: 'Pants & Shorts'
-            },
-            {
-              uid: 'MzQ=',
-              name: 'Skirts'
-            }
-          ]
-        },
-        {
-          uid: 'MTk=',
-          name: 'Tops',
-          children: [
-            {
-              uid: 'MjI=',
-              name: 'Blouses & Shirts'
-            },
-            {
-              uid: 'MjU=',
-              name: 'Sweaters'
-            }
-          ]
-        },
-        {
-          uid: 'Mzc=',
-          name: 'Dresses',
-          children: []
-        },
-        {
-          uid: 'Nw==',
-          name: 'Accessories',
-          children: [
-            {
-              uid: 'MTA=',
-              name: 'Belts'
-            },
-            {
-              uid: 'MTM=',
-              name: 'Jewelry'
-            },
-            {
-              uid: 'MTY=',
-              name: 'Scarves'
-            }
-          ]
-        },
-        {
-          uid: 'NDA=',
-          name: 'Shop The Look',
-          children: [
-            {
-              uid: 'NDM=',
-              name: 'Minimalist Sensibility'
-            },
-            {
-              uid: 'NDY=',
-              name: 'Outside the Lines'
-            },
-            {
-              uid: 'NDk=',
-              name: 'Carefree Days'
-            },
-            {
-              uid: 'NTI=',
-              name: 'Perfectly Beachy'
-            },
-            {
-              uid: 'NTU=',
-              name: 'Retire your LBD'
-            },
-            {
-              uid: 'NTg=',
-              name: 'Timeless Sophistication'
-            }
-          ]
-        }
-      ]
+      categories: []
     }
   },
+  async created () {
+    await this.fetchCategories()
+    this.categories = this.$store.state.categories
+    console.log(this.categories)
+  },
   methods: {
+    ...mapActions(['fetchCategories']),
     selectCategory (category) {
       this.$router.push(`/category/${category.name}/1`)
     }
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,39 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    categories: []
   },
   mutations: {
+    setCategories (state, categories) {
+      state.categories = categories
+    }
   },
   actions: {
+    async fetchCategories ({ commit }) {
+      await fetch('https://venia.magento.com/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: `
+            query{
+              categoryList{
+                children {
+                  uid,
+                  name,
+                  children {
+                    uid,
+                    name
+                  }
+                }
+              }
+            }
+          `
+        })
+      })
+        .then(response => response.json())
+        .then(data => commit('setCategories', data.data.categoryList[0].children))
+    }
   },
-  modules: {
+  getters: {
   }
 })
